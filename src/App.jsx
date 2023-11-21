a065e3e87da8d858d99288b646138f0f39fb3a47
Aliaskari2002 <abilong062003@gmail.com>
2023-11-22 00:37:04 +0800

    ini adalah comment percobaan

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,34 @@
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Admin from "./pages/Admin/Dashboard/Dashboard";
+import Transaction from "./pages/Admin/Transaction/transaksi";
+import Pendaftaran from "./pages/Admin/Pendaftaran/pendaftaran";
+import Notifikasi from "./pages/Admin/Notifikasi/Notifikasi";
+
+import Kategori from "./pages/Admin/MasterData/Kategori/Kategori";
+import User from "./pages/Admin/MasterData/User/user";
+import Produk from "./pages/Admin/MasterData/Produk/produk";
+import Promo from "./pages/Admin/MasterData/Promo/promo";
+import Metode from "./pages/Admin/MasterData/Metode/Metode";
 
 function App() {
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
-  )
+    <div>
+      <BrowserRouter>
+        <Routes>
+          <Route element={<Admin />} path="/admin/dashboard" />
+          <Route element={<Kategori />} path="/admin/kategori" />
+          <Route element={<User />} path="/admin/user" />
+          <Route element={<Produk />} path="/admin/Produk" />
+          <Route element={<Promo />} path="/admin/Promo" />
+          <Route element={<Metode />} path="/admin/Metode" />
+          <Route element={<Transaction />} path="/admin/transaksi" />
+          <Route element={<Pendaftaran />} path="/admin/pendaftaran" />
+          <Route element={<Notifikasi />} path="/admin/Notifikasi" />
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
 }
 
-export default App
+export default App;
---
+++ src/pages/Admin/Dashboard/Dashboard.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import SideBar from "../../../components/SideBar";
+
+function Admin() {
+  return (
+    <div className="bg-[#DDE5E9]">
+      <div className="flex flex-row ">
+        <SideBar />
+        <div className="bg-slate-950 w-full">
+          <h1 className="text-white">Dashboard</h1>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Admin;
